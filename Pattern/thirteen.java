dba91a8d4b5223e72c312b9aad12408678f6440b
Harsh <harsh20create@gmail.com>
2025-05-09 21:49:55 +0530

    add

---
+++ Pattern/thirteen.java
@@ -0,0 +1,14 @@
+package Pattern;
+
+public class thirteen {
+    public static void main(String[] args) {
+        int e=1;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<i+1;j++){
+            System.out.print(e+" ");
+            e++;
+        }
+        System.out.println();
+    }
+    }
+}
---
+++ Pattern/tweleveth.java
@@ -0,0 +1,18 @@
+package Pattern;
+
+public class tweleveth {
+public static void main(String[] args) {
+    for(int i=1;i<5;i++){
+        for(int j=1;j<i+1;j++){
+            System.out.print(j);
+        }
+        for (int j = 1; j <= 2 * (4 - i); j++) {
+            System.out.print(" ");
+        }
+        for(int j=i;j>0;j--){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+}
+}
