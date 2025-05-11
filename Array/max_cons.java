ab584b3ec3d6f44f3102804a1551026dbef36be2
Harsh <harsh20create@gmail.com>
2025-05-12 00:07:34 +0530

    add

---
+++ Array/max_cons.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class max_cons {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int t=0;
+        int max=0;
+        for(int num:nums){
+            if(num==1){
+                t++;
+            }
+            else{
+                max=Math.max(t,max);
+                t=0;
+            }
+            
+        }
+        max=Math.max(t,max);
+        return max;
+    }
+}
