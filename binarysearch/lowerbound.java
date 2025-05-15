235151c841e7f9c9e209cc526bcb555b3d139081
Harsh <harsh20create@gmail.com>
2025-05-15 23:43:44 +0530

    add

---
+++ binarysearch/lowerbound.java
@@ -0,0 +1,16 @@
+package binarysearch;
+
+public class lowerbound {
+    public int lowerBound(int[] nums, int x) {
+       int left=0,right=nums.length-1;
+       while(left<right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]<x){
+            left=mid+1;
+        }else{
+            right=mid;
+        }
+       }
+       return left;
+     }
+}
---
+++ binarysearch/upperbound.java
@@ -0,0 +1,17 @@
+package binarysearch;
+
+public class upperbound {
+    public int upperBound(int[] nums, int x) {
+        int left=0,right=nums.length-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]<=x){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        return left;
+    }
+}
