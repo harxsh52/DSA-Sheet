ea15074e174171ac05c0e51e3ec8e18c7833271f
harxsh52 <harsh20create@gmail.com>
2025-05-21 00:54:42 +0530

    add

---
+++ String/easy/max_depth.java
@@ -0,0 +1,21 @@
+package String.easy;
+
+public class max_depth {
+    public int maxDepth(String s) {
+        int max = 0;
+        int curr = 0;
+
+        for(int i =0 ; i< s.length();i++){
+            if(s.charAt(i)=='('){
+                curr++;
+              max = Math.max(curr,max);
+            }
+
+            else if(s.charAt(i)==')'){
+                curr--;
+            }
+        }
+
+        return max;
+    }
+}
