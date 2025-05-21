d19ebefcda91c455f7e1901a78343fcc306f36b7
harxsh52 <harsh20create@gmail.com>
2025-05-22 00:16:28 +0530

    add

--- binarysearch/hard/median_of _two _sorted.java
+++ binarysearch/hard/median_of _two _sorted.java
@@ -1,54 +1,49 @@
 package binarysearch.hard;
 
-public class median_of _two _sorted {
-        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int t=nums1.length+nums2.length;
-        if(t%2==0){
-            double r=(double)kthElement(nums1,nums2,t/2);
-            double w=(double)kthElement(nums1,nums2,t/2+1);
-            return (r+w)/2;
-        }else{
-            double r=(double)kthElement(nums1,nums2,t/2+1);
+public class MedianOfTwoSorted {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int t = nums1.length + nums2.length;
+        if (t % 2 == 0) {
+            double r = (double) kthElement(nums1, nums2, t / 2);
+            double w = (double) kthElement(nums1, nums2, t / 2 + 1);
+            return (r + w) / 2;
+        } else {
+            double r = (double) kthElement(nums1, nums2, t / 2 + 1);
             return r;
         }
     }
     public int kthElement(int[] a, int[] b, int k) {
-      int i=0,j=0;
-      int t=0;
-      int r=0;
-      while(i<a.length && j<b.length){
-        if(a[i]<b[j]){
+        int i = 0, j = 0;
+        int t = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                t++;
+                if (t == k) {
+                    return a[i];
+                }
+                i++;
+            } else {
+                t++;
+                if (t == k) {
+                    return b[j];
+                }
+                j++;
+            }
+        }
+        while (i < a.length) {
             t++;
-            
-            if(t==k){
+            if (t == k) {
                 return a[i];
             }
             i++;
-        }else{
+        }
+        while (j < b.length) {
             t++;
-            
-            if(t==k){
+            if (t == k) {
                 return b[j];
             }
             j++;
         }
-      }
-      while(i<a.length){
-        t++;
-        
-        if(t==k){
-            return a[i];
-        }
-        i++;
-      }
-      while(j<b.length){
-        t++;
-
-        if(t==k){
-            return b[j];
-        }
-        j++;
-      }
-      return -1;
+        return -1; // Not found
     }
 }
--- language/if_else.java
+++ language/if_else.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 
 public class if_else {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the Number:");
-        int num=sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the Number:");
+            int num = sc.nextInt();
 
-        if(num%2==0){
-            System.out.println("Number is Even");
-        }
-        else{
-            System.out.println("Number is odd");
+            if (num % 2 == 0) {
+                System.out.println("Number is Even");
+            } else {
+                System.out.println("Number is odd");
+            }
         }
     }
-
 }
