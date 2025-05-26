11bf39e4bbe50077a19283283014cac8f8200c10
harxsh52 <harsh20create@gmail.com>
2025-05-27 00:04:33 +0530

    add

--- Sorting/insertion.java
+++ Sorting/insertion.java
@@ -17,4 +17,24 @@ public class insertion {
             arr[j + 1] = key;
         }
     }
+    public static void instertion(int[] arr,int i, int n) {
+        if (i >= n) {
+            return;
+        }
+        int key = arr[i];
+        int j = i - 1;
+
+        // Shift larger elements to the right
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // Insert key at the correct position
+        arr[j + 1] = key;
+
+        // Recursive call for the next element
+        instertion(arr, i + 1, n);
+
+    }
 }
