a6e403540e89f40084c84bdb807aafef5bd55845
harxsh52 <harsh20create@gmail.com>
2025-05-28 23:04:14 +0530

    add

--- Array/easy/union.java
+++ Array/easy/union.java
@@ -4,18 +4,23 @@ import java.util.HashSet;
 
 public class union {
     public int[] unionArray(int[] nums1, int[] nums2) {
-        HashSet<Integer> set=new HashSet<>();
-        for(int num:nums1){
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums1) {
             set.add(num);
         }
-        for(int num:nums2){
+
+        for (int num : nums2) {
             set.add(num);
         }
-        int[] result=new int[set.size()];
-        int i=0;
-        for(int num:set){
-            result[i++]=num;
+
+        int[] result = new int[set.size()];
+        int index = 0;
+
+        for (int num : set) {
+            result[index++] = num;
         }
+
         return result;
     }
 }
