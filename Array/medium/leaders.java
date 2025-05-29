af9a8e9de89fec7b343b6a987c5dde34ac67f688
harxsh52 <harsh20create@gmail.com>
2025-05-29 23:55:28 +0530

    add

--- Array/medium/leaders.java
+++ Array/medium/leaders.java
@@ -4,19 +4,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class leaders {
-    class Solution {
     public ArrayList<Integer> leaders(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
-        list.add(nums[nums.length-1]);
-        int max=nums[nums.length-1];
-        for(int i=nums.length-2;i>-1;i--){
-            if(nums[i]>max){
-                list.add(nums[i]);
-                max=nums[i];
+        ArrayList<Integer> leader = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > max) {
+                leader.add(nums[i]);
+                max = nums[i];
             }
         }
-         Collections.reverse(list);
-        return list;
-    }
+
+        // Reverse the list to return in correct order
+        Collections.reverse(leader);
+
+        return leader;
+    }    public static void main(String[] args) {
+        leaders l = new leaders();
+        int[] nums = {16, 17, 4, 3, 5, 2};
+        ArrayList<Integer> result = l.leaders(nums);
+        System.out.println(result); // Output: [17, 5, 2]
+        int[] nums2 = {1, 2, 3, 4, 0};
 }
 }
---
+++ Array/medium/roatate.java
@@ -0,0 +1,27 @@
+package Array.medium;
+
+public class roatate {
+    public void rotate(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            reverse(matrix[i]);
+        }
+    }
+    public static void reverse(int[] matrix){
+        int left=0,right=matrix.length-1;
+        while(left<right){
+            matrix[left]=matrix[left]^matrix[right];
+            matrix[right]=matrix[left]^matrix[right];
+            matrix[left]=matrix[left]^matrix[right];
+            left++;
+            right--;
+            
+        }
+    }
+}
