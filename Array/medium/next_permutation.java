3983779cd69a13c37e9367dc007da8f7d4da4e7f
harxsh52 <harsh20create@gmail.com>
2025-05-31 10:54:01 +0530

    add

--- Array/medium/next_permutation.java
+++ Array/medium/next_permutation.java
@@ -2,32 +2,30 @@ package Array.medium;
 
 public class next_permutation {
         public void nextPermutation(int[] nums) {
-        int i=nums.length-2;
-        while(i>=0 && nums[i]>=nums[i+1]){
+        int n=nums.length;
+        int i=n-2;
+        while(i>=0 && nums[i+1]<=nums[i]){
             i--;
         }
-        if (i >= 0) {
-            int j = nums.length - 1;
-
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-
-            // Swap nums[i] and nums[j]
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+        if(i>=0){
+        int j=nums.length-1;
+        while(j>=i && nums[j]<=nums[i]){
+            j--;
         }
-        reverse(nums, i + 1, nums.length - 1);
-        return;
+        nums[i]=nums[i]^nums[j];
+        nums[j]=nums[i]^nums[j];
+        nums[i]=nums[i]^nums[j];
+        }
+        
+        reverse(nums,i+1,nums.length-1);
     }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start++] = nums[end];
-            nums[end--] = temp;
+    private void reverse(int[] nums,int i,int j){
+        while(i<j){
+            nums[i]=nums[i]^nums[j];
+            nums[j]=nums[i]^nums[j];
+            nums[i]=nums[i]^nums[j];
+            i++;
+            j--;
         }
-        return;
     }
 }
