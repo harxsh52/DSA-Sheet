23ffd37c745b70e8d96e47c8b90205b47097acc6
harxsh52 <harsh20create@gmail.com>
2025-06-03 17:14:41 +0530

    addmessage

--- stack_queue/practice/practice.java
+++ stack_queue/practice/practice.java
@@ -2,6 +2,26 @@ package stack_queue.practice;
 import java.util.*;
 
 public class practice {
+
+    //Stack valid parentheses
+    public static boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            } else {
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if((c==')' && top!='(') || (c=='}' && top!='{') || (c==']' && top!='[')){
+                    return false;
+                }
+            }
+        }
+                    if(stack.isEmpty()) {
+                return true; // All parentheses matched
+            }
+            return false; // Some parentheses are unmatched
+    }
     //function to reverse a string using stack
     public static void reverseStringUsingStack(String str) {
         Stack<Character> stack= new Stack<>();
@@ -22,6 +42,34 @@ public class practice {
         }
         System.out.println("Reversed String: " + reversedStr.toString());
         
+    }
+    //Function reverses a stack using recursion
+    public static void reverseStack(Stack<Integer> stack){
+        if(stack.isEmpty()){
+            return;
+        }
+        int temp = stack.pop();
+        reverseStack(stack);
+        pushAtBottom(stack, temp);
+    }
+
+    //Next Greater element
+    public static void nextGreaterElement(int[] arr) {
+        int n=arr.length;
+        Stack<Integer> stack=new Stack<>();
+        int[] result=new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=arr[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                result[i]=-1;
+            }else{
+                result[i]=stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        System.out.println("Next Greater Element: " + Arrays.toString(result));
     }
     // Function to push an element at the bottom of the stack
     // using recursion
@@ -48,6 +96,12 @@ public class practice {
         System.out.println("Stack after pushing 5 at bottom: " + stack);
         String str = "Hello, World!";
         reverseStringUsingStack(str);
-        // Example usage of reverseStringUsingStack
+        // Example usage of reverseStringUsingStack    
+        System.out.println("Reversed String: " + str);
+        int[] arr = {4, 5, 2, 10, 8};
+
+        nextGreaterElement(arr);
+        isValid("()[]{}"); // Example usage of isValid
+        System.out.println("Is valid parentheses: " + isValid("()[]{"));
     }
 }
