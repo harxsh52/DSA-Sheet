d21558f1c80de1bdf2aeb31c8523671cf04a7886
harxsh52 <harsh20create@gmail.com>
2025-06-04 22:23:38 +0530

    add

---
+++ Recursion/practice/fibonacci.java
@@ -0,0 +1,16 @@
+package Recursion.practice;
+
+public class fibonacci {
+    public static int fibo(int n){
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+    public static void main(String[] args) {
+        int t=fibo(4);
+        System.out.println(t);
+    }
+}
