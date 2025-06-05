190b35ee7571a0967898edea85e9402b6ea5573c
harxsh52 <harsh20create@gmail.com>
2025-06-05 21:50:16 +0530

    add

---
+++ practice/genrate_parenthsis.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class genrate_parenthsis {
+    public List<String> generateParenthesis(int n) {
+        List<String> s=new ArrayList<>();
+        genrate(n,"",0,0,s);
+        return s;
+    }
+    public static void genrate(int n,String sq,int open,int close,List<String> s){
+        if(sq.length()==n*2){
+            s.add(sq);
+            return;
+        }
+        if(open<n){
+            genrate(n,sq+'(',open+1,close,s);
+        }
+        if(close<open){
+            genrate(n,sq+')',open,close+1,s);
+        }
+    }
+}
