37dc48a65973aa03a7547cb683d455e5213eabba
harxsh52 <harsh20create@gmail.com>
2025-06-17 20:01:49 +0530

    add

--- Binarytree/practice/Binarytree.java
+++ Binarytree/practice/Binarytree.java
@@ -1,7 +1,8 @@
 package Binarytree.practice;
 import java.util.*;
 
-public class preorder {
+public class Binarytree {
+    //Class to define the node
     static class Node{
         int data;
         Node left;
@@ -11,6 +12,7 @@ public class preorder {
             right=left=null;
         }
     }
+    //Class to make the BinaryTree
     static class BinaryTree{
         static int idx=-1;
         public static Node buildTree(int[] nodes){
@@ -25,6 +27,8 @@ public class preorder {
         }
 
     }
+
+    //preorder print
     public static void preorderprint(Node root){
         if(root==null){
             return;
@@ -33,6 +37,8 @@ public class preorder {
         preorderprint(root.left);
         preorderprint(root.right);
     }
+
+    //print inorder
     public static void inorderprint(Node root){
         if(root==null){
             return;
@@ -41,6 +47,8 @@ public class preorder {
         System.out.print(root.data +" ");
         inorderprint(root.right);
     }
+
+    //print postorder
     public static void postorderprint(Node root){
         if(root==null){
             return;
@@ -49,6 +57,8 @@ public class preorder {
         postorderprint(root.right);
         System.out.print(root.data);
     }
+
+    //print levelorder
     public static void levelorder(Node root){
         if(root==null){
             return;
@@ -77,6 +87,33 @@ public class preorder {
         }
         return;
     }
+    //height
+    public static int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        int lh=height(root.left);
+        int rh=height(root.right);
+        return Math.max(lh,rh)+1;
+    }
+    //count the nodes
+    public static int count(Node root){
+        if(root==null){
+            return 0;
+
+        }
+        int lcount=count(root.left);
+        int rcount=count(root.right);
+        return lcount+rcount+1;
+    }
+
+    //sum of the node value
+    public static int ssum(Node root){
+        if(root==null){
+            return 0;
+        }
+        return ssum(root.left)+ssum(root.right)+root.data;
+    }
     public static void main(String[] args) {
         int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
@@ -89,5 +126,9 @@ public class preorder {
         postorderprint(root);
         System.out.println("");
         levelorder(root);
+        System.out.println("");
+        System.out.println(height(root));
+        System.out.println(count(root));
+        System.out.println(ssum(root));
     }
 }
