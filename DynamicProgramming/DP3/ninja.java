7e5b373afc3aa1c8b72fd727a1276ce1cfc80bc2
harxsh52 <harsh20create@gmail.com>
2025-07-11 22:03:24 +0530

    add

--- DynamicProgramming/DP3/ninja.java
+++ DynamicProgramming/DP3/ninja.java
@@ -4,9 +4,10 @@ public class ninja {
     public int maximumPoints(int arr[][]) {
         // code here
         int[][] dp=new int[arr.length][4];
-        dp[0][0]=Math.max(arr[0][1],arr[0][2]);
+        dp[0][0]=Math.max(arr[0][0],arr[0][1]);
         dp[0][1]=Math.max(arr[0][0],arr[0][2]);
-        dp[0][2]=Math.max(arr[0][1],arr[0][0]);
+        dp[0][2]=Math.max(arr[0][1],arr[0][2]);
+        dp[0][3]=Math.max(arr[0][0],Math.max(arr[0][1],arr[0][2]));
         for(int i=1;i<arr.length;i++){
             for(int prev=0;prev<4;prev++){
                 dp[i][prev]=0;
---
+++ DynamicProgramming/DP4/subsetsumeqauals.java
@@ -0,0 +1,25 @@
+package DynamicProgramming.DP4;
+
+public class subsetsumeqauals {
+    static Boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        boolean[][] dp=new boolean[arr.length][sum+1];
+        for(int i=0;i<arr.length;i++){
+            dp[i][0]=true;
+        }
+        if(arr[0]<=sum){
+            dp[0][arr[0]]=true;
+        }
+        for(int i=1;i<arr.length;i++){
+            for(int j=1;j<=sum;j++){
+                boolean notpick=dp[i-1][j];
+                boolean pick=false;
+                if(arr[i]<=j){
+                    pick=dp[i-1][j-arr[i]];
+                }
+                dp[i][j]=pick || notpick;
+            }
+        }
+        return dp[arr.length-1][sum];
+    }
+}
