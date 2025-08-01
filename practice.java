08b14a2d1377aefb76d7af3130c7fb9ec96b3b51
harxsh52 <harsh20create@gmail.com>
2025-08-01 23:10:28 +0530

    add

--- practice.java
+++ practice.java
@@ -44,7 +44,7 @@ public class practice {
         return dp[n - 1][k];
     }
     public static void main(String[] args) {
-        // This is a simple Java program
+    
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] arr=new int[n];
